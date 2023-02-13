340a9f50dfa1f410c81ba8ab4308602089c46743
Agung Gunawan <agg.nst772@gmail.com>
2023-02-13 21:50:42 +0700

    update Memorize prototype

--- Assets/Scripts/KartuController.cs
+++ Assets/Scripts/KartuController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class KartuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string color;
+    public bool flip, clear;
+
+    private void Start()
     {
-        
+        ChangeColors();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeColors()
     {
-        
+        if (color == "red")
+        {
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else if (color == "blue")
+        {
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        else if (color == "yellow")
+        {
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            print("Isi warna objectnya cuy");
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        ClickMouse();
+    }
+
+    public void ClickMouse()
+    {
+        if (clear) return;
+
+        if (GameplayMemorize.instance.select1 == "" || GameplayMemorize.instance.select2 == "")
+        {
+            flip = true;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            GameplayMemorize.instance.SelectCard(color);
+        }
+        else if (flip)
+        {
+            flip = false;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            GameplayMemorize.instance.select1 = "";
+            GameplayMemorize.instance.select2 = "";
+        }
+        print(gameObject.name);
+    }
+
+    public void FlipBack()
+    {
+        flip = false;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
