db0847847a7c02614ac5de35ac59c28b1f09f276
AkuFauzi <fauziabdillah009@gmail.com>
2023-02-23 14:23:06 +0700

    UpdateScriptPG

--- Assets/Scripts/GameplayPilihanGanda.cs
+++ Assets/Scripts/GameplayPilihanGanda.cs
@@ -26,6 +26,12 @@ public class GameplayPilihanGanda : MonoBehaviour
     public List<ListPertanyaan> listPertanyaanBab2;
     public List<ListPertanyaan> listPertanyaanBab3;
     public List<ListPertanyaan> listPertanyaanBab4;
+    public List<ListPertanyaan> listPertanyaanBab5;
+    public List<ListPertanyaan> listPertanyaanBab6;
+    public List<ListPertanyaan> listPertanyaanBab7;
+    public List<ListPertanyaan> listPertanyaanBab8;
+    public List<ListPertanyaan> listPertanyaanBab9;
+    public List<ListPertanyaan> listPertanyaanBab10;
     private void Awake()
     {
         instance = this;
@@ -75,6 +81,46 @@ public class GameplayPilihanGanda : MonoBehaviour
             {
                 SpawnPilihanGanda(listPertanyaanBab2[urutanPertanyaan].pertanyaan,
                    listPertanyaanBab2[urutanPertanyaan].a, listPertanyaanBab2[urutanPertanyaan].b, listPertanyaanBab2[urutanPertanyaan].c);
+            }
+            else if (bab == 3)
+            {
+                SpawnPilihanGanda(listPertanyaanBab3[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab3[urutanPertanyaan].a, listPertanyaanBab3[urutanPertanyaan].b, listPertanyaanBab3[urutanPertanyaan].c);
+            }
+            else if (bab == 4)
+            {
+                SpawnPilihanGanda(listPertanyaanBab4[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab4[urutanPertanyaan].a, listPertanyaanBab4[urutanPertanyaan].b, listPertanyaanBab4[urutanPertanyaan].c);
+            }
+            else if (bab == 5)
+            {
+                SpawnPilihanGanda(listPertanyaanBab5[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab5[urutanPertanyaan].a, listPertanyaanBab5[urutanPertanyaan].b, listPertanyaanBab5[urutanPertanyaan].c);
+            }
+            else if (bab == 6)
+            {
+                SpawnPilihanGanda(listPertanyaanBab6[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab6[urutanPertanyaan].a, listPertanyaanBab6[urutanPertanyaan].b, listPertanyaanBab6[urutanPertanyaan].c);
+            }
+            else if (bab == 7)
+            {
+                SpawnPilihanGanda(listPertanyaanBab7[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab7[urutanPertanyaan].a, listPertanyaanBab7[urutanPertanyaan].b, listPertanyaanBab7[urutanPertanyaan].c);
+            }
+            else if (bab == 8)
+            {
+                SpawnPilihanGanda(listPertanyaanBab8[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab8[urutanPertanyaan].a, listPertanyaanBab8[urutanPertanyaan].b, listPertanyaanBab8[urutanPertanyaan].c);
+            }
+            else if (bab == 9)
+            {
+                SpawnPilihanGanda(listPertanyaanBab9[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab9[urutanPertanyaan].a, listPertanyaanBab9[urutanPertanyaan].b, listPertanyaanBab9[urutanPertanyaan].c);
+            }
+            else if (bab == 10)
+            {
+                SpawnPilihanGanda(listPertanyaanBab10[urutanPertanyaan].pertanyaan,
+                   listPertanyaanBab10[urutanPertanyaan].a, listPertanyaanBab10[urutanPertanyaan].b, listPertanyaanBab10[urutanPertanyaan].c);
             }
 
         }
