ecad1de7a4cac8c09a585b1056c90fff35ade9c7
AkuFauzi <fauziabdillah009@gmail.com>
2023-02-28 10:55:49 +0700

    UpadteSoalBS

--- Assets/Scripts/GameplayBenarSalah.cs
+++ Assets/Scripts/GameplayBenarSalah.cs
@@ -23,6 +23,15 @@ public class GameplayBenarSalah : MonoBehaviour
         public string jawaban;
     }
     public List<ListPertanyaan> listPertanyaanBab1;
+    public List<ListPertanyaan> listPertanyaanBab2;
+    public List<ListPertanyaan> listPertanyaanBab3;
+    public List<ListPertanyaan> listPertanyaanBab4;
+    public List<ListPertanyaan> listPertanyaanBab5;
+    public List<ListPertanyaan> listPertanyaanBab6;
+    public List<ListPertanyaan> listPertanyaanBab7;
+    public List<ListPertanyaan> listPertanyaanBab8;
+    public List<ListPertanyaan> listPertanyaanBab9;
+    public List<ListPertanyaan> listPertanyaanBab10;
     private void Awake()
     {
         instance = this;
@@ -69,9 +78,40 @@ public class GameplayBenarSalah : MonoBehaviour
             }
             else if (bab == 2)
             {
-
+                SpawnBenarSalah(listPertanyaanBab2[urutanPertanyaan].pertanyaan, listPertanyaanBab2[urutanPertanyaan].jawaban);
             }
-
+            else if (bab == 3)
+            {
+                SpawnBenarSalah(listPertanyaanBab3[urutanPertanyaan].pertanyaan, listPertanyaanBab3[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 4)
+            {
+                SpawnBenarSalah(listPertanyaanBab4[urutanPertanyaan].pertanyaan, listPertanyaanBab4[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 5)
+            {
+                SpawnBenarSalah(listPertanyaanBab5[urutanPertanyaan].pertanyaan, listPertanyaanBab5[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 6)
+            {
+                SpawnBenarSalah(listPertanyaanBab6[urutanPertanyaan].pertanyaan, listPertanyaanBab6[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 7)
+            {
+                SpawnBenarSalah(listPertanyaanBab7[urutanPertanyaan].pertanyaan, listPertanyaanBab7[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 8)
+            {
+                SpawnBenarSalah(listPertanyaanBab8[urutanPertanyaan].pertanyaan, listPertanyaanBab8[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 9)
+            {
+                SpawnBenarSalah(listPertanyaanBab9[urutanPertanyaan].pertanyaan, listPertanyaanBab9[urutanPertanyaan].jawaban);
+            }
+            else if (bab == 10)
+            {
+                SpawnBenarSalah(listPertanyaanBab10[urutanPertanyaan].pertanyaan, listPertanyaanBab10[urutanPertanyaan].jawaban);
+            }
         }
 
     }
