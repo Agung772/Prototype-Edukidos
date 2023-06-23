0de004a2d432a1625eec762a7c4658786afefbc9
Agung Gunawan <agg.nst772@gmail.com>
2023-06-23 15:25:39 +0700

    up

--- Assets/Scripts/LoadDataBab.cs
+++ Assets/Scripts/LoadDataBab.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class LoadDataBab : MonoBehaviour
 {
     public int bab;
+    public Text totalBintangText;
     public Text cTDText, sBText, pGText, BSText;
 
     private void Start()
@@ -15,9 +16,19 @@ public class LoadDataBab : MonoBehaviour
 
     public void LoadBabText()
     {
+        /*
         cTDText.text = "CTD : " + PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreConnectingTheDot + bab + SaveManager.instance.GameSave.codeSave);
         sBText.text = "SB : " + PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreSpellingBee + bab + SaveManager.instance.GameSave.codeSave);
         pGText.text = "PG : " + PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScorePilihanGanda + bab + SaveManager.instance.GameSave.codeSave);
         BSText.text = "BS : " + PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreBenarSalah + bab + SaveManager.instance.GameSave.codeSave);
+        */
+
+        int cTD = PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreConnectingTheDot + bab + SaveManager.instance.GameSave.codeSave);
+        int sB = PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreSpellingBee + bab + SaveManager.instance.GameSave.codeSave);
+        int pG = PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScorePilihanGanda + bab + SaveManager.instance.GameSave.codeSave);
+        int bS = PlayerPrefs.GetInt(SaveManager.instance.GameSave._ScoreBenarSalah + bab + SaveManager.instance.GameSave.codeSave);
+
+        int totalBintang = cTD + sB + pG + bS;
+        totalBintangText.text = "" + totalBintang;
     }
 }
